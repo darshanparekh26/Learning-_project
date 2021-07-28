f80d4bb61342134c6fc19c89be9ab0a3b17e34cd
darshanparekh26 <53456632+darshanparekh26@users.noreply.github.com>
2021-07-28 13:13:48 +0530

    Add files via upload

---
+++ count1.java
@@ -0,0 +1,25 @@
+class count1
+{
+public static void main(String ar[])
+{
+
+int e=0;
+int n=10,c=4,d=3;
+
+
+for(int i=c;i<=n;i++)
+{
+
+i=i-d;
+i=(i+c)-1;
+e++;
+}
+System.out.println(e);
+}
+
+
+}
+
+
+
+
